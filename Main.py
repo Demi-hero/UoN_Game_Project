cd5061a170e122b1fa803ea1cc53e1d2f4add97f
ExcessGravitas <45288304+ExcessGravitas@users.noreply.github.com>
2018-11-29 19:33:05 +0000

    import improvements
    Changed the imports so no longer import *
    Also moved the start screen logic to event handler

--- Main.py
+++ Main.py
@@ -1,11 +1,10 @@
 import pygame as pyg
 import os
-# change these imports to not import *
-from gamedata import *
-from eventhandler import *
+import gamedata as gd
+import eventhandler as eh
 
 
-class Main(HandleEvent):
+class Main(eh.HandleEvent):
 
     def __init__(self):
         self.startup = True
@@ -14,56 +13,36 @@ class Main(HandleEvent):
         self.lives = 3
         self.score = 0
         self.clock = pyg.time.Clock()
-        self.framerate = 100
-        # objects created inside the class. Not sure if this is a good thing or not?
-        self.Files = FileStore()
-        self.Board = Background()
-        self.Player1 = Player()
-        self.Bullet1 = Bullet()
-        self.Alien1 = Alien()
-
-    # will need refactoring to match rest of code style is how the code handles start up
-    def on_startup(self):
-        self.Board.update()
-        # self.message_display("Highscores :",xloc=.35)
-        # self.message_display(" Lore :", xloc=.65)
-        self.message_display("Movement", yloc=.57, xloc=.26)
-        self.message_display("Shoot", yloc=.65, xloc=.7)
-        self.Board.screen.blit(self.Files.title, (221, 0))
-        self.Board.screen.blit(self.Files.arrows, (160, 320))
-        # gamedata.background.screen.blit(Game_Data.startup.h, (425, 228))
-        # gamedata.background.screen.blit(Game_Data.startup.l, (675, 228))
-        self.Board.screen.blit(self.Files.space, (591, 375))
-        self.message_display("Press Shoot to Start", .9, font_size=35)
-        pyg.display.flip()
-        while self.startup:
-            for event in pyg.event.get():
-                self.on_event(event)
-            pyg.display.flip()
+        self.framerate = 120
 
     def execute(self):
         pyg.init()
         # creating the screen and game objects
+        Files = gd.FileStore()
+        Board = gd.Background()
+        Player1 = gd.Player()
+        Bullet1 = gd.Bullet()
+        Alien1 = gd.Alien()
 
         while self.startup:
-            self.on_startup()
+            self.on_startup(Board, Files)
 
         # main game loop
         while self.running:
             # taking the player input, passing to event handler
             for event in pyg.event.get():
-                self.on_event(event, self.Player1, self.Bullet1)
-            self.player_movement(self.Player1)
+                self.on_event(event, Player1, Bullet1, Files)
+            self.player_movement(Player1)
 
             # updating the object states, and drawing to screen (see gamedata)
             if not self.paused:
-                self.Board.update()
-                self.Player1.update()
-                self.Bullet1.update()
+                Board.update()
+                Player1.update()
+                Bullet1.update()
                 # passes main (self) to alien update and detect_collision to update score and lives
-                self.Alien1.update(self)
+                Alien1.update(self)
                 # detecting collisions between aliens and bullets, and aliens and player
-                self.Alien1.detect_collisions(self.Player1, self.Bullet1, self)
+                Alien1.detect_collisions(Player1, Bullet1, self)
 
                 # display lives and score at top of screen
                 self.message_display("Score:{}".format(self.score), 0.03, 0.1, 20)
@@ -71,14 +50,14 @@ class Main(HandleEvent):
 
                 # if out of lives - game over (see eventhandler)
                 if self.lives < 1:
-                    self.gameover(self.Player1, self.Bullet1, self.Alien1)
+                    self.gameover(Board, Player1, Bullet1, Alien1, Files)
 
                 # update the display
                 pyg.display.flip()
                 self.clock.tick(self.framerate)
 
-
 App = Main()
 App.execute()
 pyg.quit()
 os._exit(0)
+
