5a78bf59cbabdb233b7136418f9bd76518053753
Nate <scott.pearce3@gmail.com>
2018-11-30 13:57:28 +0000

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,8 +1,13 @@
 import time
 starttime=time.time()
+power_up = False
+print(starttime)
 while True:
-    if (time.time() - starttime)//1 == 10:
-        print(" I am a power up")
+    if (time.time() - starttime)//1 == 10 and not power_up:
+        print("I am a power up")
+        power_up = True
         starttime = time.time()
-
-    #print((time.time() - starttime)//1)
+    elif (time.time() - starttime)//1 == 10 and  power_up:
+        print("Powerup despawn?")
+        starttime = time.time()
+        power_up = False
