3c529cdd0a0536543a2a42259fa0da4967d18de4
ExcessGravitas <45288304+ExcessGravitas@users.noreply.github.com>
2018-12-11 09:35:03 +0000

    Re-org and re-comment
    Re-organised the code so common elements are together (like collisions, clearing elements as the leave the screen, etc)
    Updated the comments so they now reflect the various updates
    Cleared some redundant code

--- Main.py
+++ Main.py
@@ -4,20 +4,27 @@ import av_data as av
 import gamedata as gd
 import eventhandler as eh
 import time
+
+
 class Main(eh.HandleEvent):
 
     def __init__(self):
+        # possible game states
         self.startup = True
         self.running = True
         self.paused = False
+        # starting values
         self.lives = 3
         self.bombs = 1
         self.score = 0
+        self.kill_score = 50
+        self.penalty_score = -10
+        self.wavenum = 1
+        # basics game needs to run
         self.white = (255,255,255)
         self.clock = pyg.time.Clock()
         self.starttime = 0
         self.framerate = 100
-        self.wavenum = 1
         # creating the screen and game objects
         self.all_sprites = pyg.sprite.Group()
         self.Files = av.FileStore()
@@ -30,60 +37,63 @@ class Main(eh.HandleEvent):
         self.alienbullets = pyg.sprite.Group()
 
 
-    def new_powerup(self):
-        power_up = gd.PowerUp(self)
-        self.power_ups.add(power_up)
-        self.all_sprites.add(power_up)
-
     def execute(self):
-
+        # spawning aliens
         self.new_alien(self.wavenum)
 
+        # startup screen
         while self.startup:
             self.on_startup(self.Board, self.Files)
         self.starttime = time.time()
 
         # main game loop
         while self.running:
-            # taking the player input, passing to event handler
 
+            # taking the player input, passing to event handler
             for event in pyg.event.get():
                 self.on_event(event, self.Files, self.Board)
             self.player_movement(self.player)
 
-            # updating the object states, and drawing to screen (see gamedata)
+
             if not self.paused:
+                # spawning powerups
                 if (time.time() - self.starttime) // 1 == 3 and len(self.power_ups) == 0:
                     self.new_powerup()
                 elif (time.time() - self.starttime) // 1 > 15:
                     self.starttime = time.time()
+
+                # updating the object states
                 self.Board.update()
                 self.all_sprites.update()
 
-                # check to see if a bullet hit a mob
+                # COLLISIONS
+                # check to see if a bullet hit an alien
                 hits = pyg.sprite.groupcollide(self.aliens, self.bullets, True, True)
                 for hit in hits:
-                    self.score += 50
+                    self.score += self.kill_score
                     self.Files.boom.play()
                     self.expl = gd.Explosion(hit.rect.center, 'lg')
                     self.all_sprites.add(self.expl)
                     self.new_alien(-6)
-
+                # check if a player picked up a powerup
                 hits = pyg.sprite.spritecollide(self.player, self.power_ups, True)
                 for hit in hits:
                     hit.power_up[3]()
-                    print("Power up collected")
-
-                # check to see if a mob hit the player
+                # check to see if an alien hit the player
                 hits = pyg.sprite.spritecollide(self.player, self.aliens, True, pyg.sprite.collide_circle)
                 for hit in hits:
                     self.player_death(hit)
+                # check to see if an alien bullet hit the player
+                hits = pyg.sprite.spritecollide(self.player, self.alienbullets, True)
+                for hit in hits:
+                    self.player_death(hit)
 
-
+                # removing the aliens and bullets as they leave the screen
                 for alien in self.aliens:
                     if alien.rect.x < 0 - alien.ln:
                         self.aliens.remove(alien)
-                        self.score -= 10
+                        # penalty for if aliens get past player
+                        self.score += self.penalty_score
                         self.new_alien( -6)
                 for bullet in self.bullets:
                     if bullet.rect.x > gd.WIDTH:
@@ -91,12 +101,8 @@ class Main(eh.HandleEvent):
                 for bullet in self.alienbullets:
                     if bullet.rect.x < (0 - bullet.ln):
                         bullet.kill()
-                hits = pyg.sprite.spritecollide(self.player, self.alienbullets, True)
-                for hit in hits:
-                    self.player_death(hit)
 
-
-
+                # drawing the scene
                 self.Board.draw()
                 self.all_sprites.draw(self.Board.screen)
 
